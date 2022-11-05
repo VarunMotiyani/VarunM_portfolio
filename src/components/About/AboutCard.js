c90e3cf5eb6a912bb1aaff5172d4d259efb2a28a
VarunMotiyani <varun.motiyani14@gmail.com>
2022-11-05 22:25:12 +0530

    initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Varun Motiyani </span>
+            from <span className="purple"> Mumbai, India.</span>
+            <br />I am a junior pursuing B.E in Information Technology in VESIT
+            Mumbai.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writting Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer"></footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
